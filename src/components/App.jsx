e6fccd1332fbe23b1c360fa2f05d77d985c8741a
Vadym8177 <vadim.8177@icloud.com>
2022-09-21 23:31:52 +0300

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,15 @@ import friends from '../friends.json';
 import transactions from '../transactions.json';
 
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
     <>
       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
